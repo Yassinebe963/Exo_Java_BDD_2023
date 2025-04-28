dcd6d512bd9f34c21e7d08cca849d47b67cb7d33
Yassinebe963 <yassinebe963@gmail.com>
2025-04-28 12:36:16 +0200

    Update TaskServlet.java

--- TaskServlet.java
+++ TaskServlet.java
@@ -1,8 +1,9 @@
-@WebServlet("/tasks")
-public class TaskServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        // Ajout tâche
-        Task task = new Task(request.getParameter("title"), ...);
-        ((List<Task>) request.getSession().getAttribute("tasks")).add(task);
-    }
+protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    Task task = new Task(
+        request.getParameter("title"),
+        request.getParameter("description"),
+        LocalDate.parse(request.getParameter("dueDate"))
+    );
+    List<Task> tasks = (List<Task>) request.getSession().getAttribute("tasks");
+    tasks.add(task);
 }
